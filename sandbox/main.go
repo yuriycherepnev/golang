eb67653791b0a21598f719f772933e4cd9ce82fc
yuriy <yuriy.cherepnev@gmail.com>
2024-12-26 17:08:49 +0300

    111

--- sandbox/main.go
+++ sandbox/main.go
@@ -20,8 +20,24 @@ func (a Aircraft) move() {
 	fmt.Println("Самолет летит")
 }
 
+func NewCar() *Car {
+	return &Car{}
+}
+
+func Add(src []int) {
+	src = append(src, 1)
+}
+
 func main() {
-	tesla := Car{}
+	arr := []int{1, 2, 3}
+	src := make([]int, 1)
+	copy(src, arr)
+
+	Add(src)
+	fmt.Println(arr) // 1 2 3
+	fmt.Println(src) // 1 1
+
+	tesla := NewCar()
 	boing := Aircraft{}
 	drive(tesla)
 	drive(boing)
