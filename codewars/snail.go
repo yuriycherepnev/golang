82fe40deee46c4cd46bf9eb395bf2cf5414f3725
yuriy <yuriy.cherepnev@gmail.com>
2025-03-21 12:27:10 +0300

    edits

--- codewars/snail.go
+++ codewars/snail.go
@@ -4,133 +4,47 @@
 упорядоченные от самых внешних элементов к среднему элементу,
 по часовой стрелке.
 */
-
 package main
 
 import "fmt"
 
-/*
-func Snail(snailMatrix [][]int) []int {
-	width, height := len(snailMatrix[0]), len(snailMatrix)
-	stepCount := width * height
+func SnailTwo(snailMatrix [][]int) []int {
+	width, height := len(snailMatrix[0]), len(snailMatrix) //ширина высота матрицы
+	stepCount := width * height                            //кол-во шагов всей матрицы
+	step := 0                                              //текущий уровень, количество шагов на уровне
+	dx, dy := 1, 0                                         //траектория движения
+	x, y := -1, 0                                          //начальная точка
 	snailNumbers := make([]int, 0, stepCount)
-	level, levelStep := 0, 0
-
+	
 	for len(snailNumbers) < stepCount {
-		if width <= 0 || height <= 0 {
-			break
-		}
-
-		x, y := calculateCoordinates(level, levelStep, width, height)
+		x += dx
+		y += dy
 		snailNumbers = append(snailNumbers, snailMatrix[y][x])
-		levelStep++
-
-		if levelStep == (2*(width+height) - 4) {
-			level++
-			levelStep = 0
+		step++
+		if step == (2*(width+height) - 4) {
+			step = 0
 			width -= 2
 			height -= 2
 		}
 	}
-
 	return snailNumbers
 }
-
-func calculateCoordinates(level, step, width, height int) (int, int) {
-	x, y := level, level
-	maxX, maxY := width+level-1, height+level-1
-
-	switch {
-	case step < maxX-level:
-		x += step
-	case step < (maxX-level)+(maxY-level):
-		x = maxX
-		y += step - (maxX - level)
-	case step < (maxX-level)*2+(maxY-level):
-		x = maxX - (step - (maxX - level) - (maxY - level))
-		y = maxY
-	default:
-		y = maxY - (step - (2*(maxX-level) + (maxY - level)))
-	}
-
-	return x, y
-}
-*/
-
-//func snail(matrix [][]int) [][]int {
-//	height := len(matrix)
-//	width := len(matrix[0])
-//
-//	level := 1
-//	stepCount := width * height
-//	levelSize := ((height + width) * 2) - 4
-//	for i := 1; i <= stepCount; i++ {
-//		x, y := calculateCoordinates(level, i, width, height)
-//		fmt.Println(x, y)
-//
-//		levelSize--
-//		if levelSize == 0 && i != stepCount {
-//			height -= 2
-//			width -= 2
-//			levelSize = ((height + width) * 2) - 4
-//			level++
-//		}
-//	}
-//
-//	return matrix
-//}
-//
-//func calculateCoordinates(level int, levelStep int, height int, width int) (int, int) {
-//	x := 1
-//	y := 1
-//	sideLength := (width + height) - 1
-//
-//	if levelStep < sideLength {
-//		if levelStep > width-1 {
-//			x = width
-//			y = y + (levelStep - width)
-//		} else {
-//			x = levelStep
-//		}
-//	} else {
-//		y = height
-//		x = width
-//		levelStep = levelStep - sideLength
-//
-//		if levelStep > width-1 {
-//			y = y - (levelStep - (width - 1))
-//		} else {
-//			x = x - levelStep
-//		}
-//	}
-//
-//	return x, y
-//}
 
 func snail(snaipMap [][]int) []int {
 	var result []int
 	lines := len(snaipMap[0])*2 - 1
 	moves := len(snaipMap[0])
-	dx, dy := 1, 0
-	x, y := -1, 0
-
+	dx, dy := 1, 0 //траектория движения
+	x, y := -1, 0  //начальная точка
 	for ; lines > 0; lines-- {
-		fmt.Println("line:", lines)
-		fmt.Println("moves:", moves)
-		fmt.Println("dx:", dx, "/ dy:", dy)
-
 		for range make([]int, moves) {
-			x += dx //1
-			y += dy //0
+			x += dx
+			y += dy
 			result = append(result, snaipMap[y][x])
-			fmt.Println("x:", x, "/ y:", y)
 		}
-
-		fmt.Println("------------------------------------")
 		moves -= 1 & dx
 		dx, dy = -dy, dx
 	}
-
 	return result
 }
 
--- codewars/test.go
+++ codewars/test.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	dx := 1
-	moves := 0
-	moves -= 1 & dx
-	fmt.Println(moves)
-	moves -= 1 & dx
-	fmt.Println(moves)
-	dx = 0
-	moves -= 1 & dx
-	moves -= 1 & dx
-	fmt.Println(moves)
+	dx, dy := 1, 0
+	fmt.Println(dx, dy)
+	dx, dy = -dy, dx
+	fmt.Println(dx, dy)
+	dx, dy = -dy, dx
+	fmt.Println(dx, dy)
+	dx, dy = -dy, dx
+	fmt.Println(dx, dy)
+	dx, dy = -dy, dx
+	fmt.Println(dx, dy)
 }
