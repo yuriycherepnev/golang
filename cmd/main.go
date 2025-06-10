4d9904db85846b7b0caef2862d730a03a906ac1d
yuriy <yuriy.cherepnev@gmail.com>
2025-06-10 21:57:48 +0300

    111

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var foo string
+	//var bar string = "Go"
+	//var (
+	//	foa string = "Go"
+	//	foe string = "Lang"
+	//)
+
+	type STR = string
+	var str STR = "Hello!"
+	fmt.Println("Hello World", str)
+}
--- sandbox/main.go
+++ sandbox/main.go
@@ -1,27 +1,79 @@
+// ./sender/main.go
+
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/streadway/amqp"
 )
 
-func fibonacci(n int) int {
-	if n == 0 {
-		return 0
-	} else if n == 1 {
-		return 1
-	} else {
-		return fibonacci(n-1) + fibonacci(n-2)
-	}
-}
 func main() {
-	start := time.Now()
-	const number = 20
-	fibNumbers := make([]int, 0, 20)
-	for i := 0; i <= number; i++ {
-		fibNumbers = append(fibNumbers, fibonacci(i))
+	// Define RabbitMQ server URL.
+	amqpServerURL := os.Getenv("AMQP_SERVER_URL")
+
+	// Create a new RabbitMQ connection.
+	connectRabbitMQ, err := amqp.Dial(amqpServerURL)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println(fibNumbers)
-	duration := time.Since(start).Seconds()
-	fmt.Println(duration)
+	defer connectRabbitMQ.Close()
+
+	// Let's start by opening a channel to our RabbitMQ
+	// instance over the connection we have already
+	// established.
+	channelRabbitMQ, err := connectRabbitMQ.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer channelRabbitMQ.Close()
+
+	// With the instance and declare Queues that we can
+	// publish and subscribe to.
+	_, err = channelRabbitMQ.QueueDeclare(
+		"QueueService1", // queue name
+		true,            // durable
+		false,           // auto delete
+		false,           // exclusive
+		false,           // no wait
+		nil,             // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new Fiber instance.
+	app := fiber.New()
+
+	// Add middleware.
+	app.Use(
+		logger.New(), // add simple logger
+	)
+
+	// Add route.
+	app.Get("/send", func(c *fiber.Ctx) error {
+		// Create a message to publish.
+		message := amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(c.Query("msg")),
+		}
+
+		// Attempt to publish a message to the queue.
+		if err := channelRabbitMQ.Publish(
+			"",              // exchange
+			"QueueService1", // queue name
+			false,           // mandatory
+			false,           // immediate
+			message,         // message to publish
+		); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	// Start Fiber API server.
+	log.Fatal(app.Listen(":3000"))
 }
