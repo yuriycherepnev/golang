aacbc9638488b81ea2d8d1cd411cb01826d6f933
yuriy <yuriy.cherepnev@gmail.com>
2025-09-10 11:03:31 +0300

    111

--- codewars/reverse_digits.go
+++ codewars/reverse_digits.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
 	count := Digitize(5555)
 	fmt.Println(count)
 }
@@ -17,30 +16,3 @@ func Digitize(number int) (slice []int) {
 		}
 	}
 }
-
-/**
-TODO первое решение с вычислением длины числа:
-
-func numberLength(number int) (count int) {
-	for number != 0 {
-		number = number / 10
-		count++
-	}
-	return count
-}
-
-func Digitize(number int) []int {
-	println(numberLength(number))
-	numberSlice := make([]int, 0, numberLength(number))
-	for {
-		digit := number % 10
-		number = number / 10
-		numberSlice = append(numberSlice, digit)
-		if number == 0 {
-			break
-		}
-	}
-	return numberSlice
-}
-
-*/
