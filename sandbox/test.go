9961fe72ad2157d837c68ecfa0f9d515fb306f03
yuriy <yuriy.cherepnev@gmail.com>
2025-10-09 17:55:57 +0300

    111

--- sandbox/test.go
+++ sandbox/test.go
@@ -27,3 +27,34 @@ func findClosestElements(arr []int, k int, x int) []int {
 
 	return arr[left : left+k]
 }
+
+/**
+package main
+
+import "fmt"
+
+func main() {
+	array := []int{2, 4, 6, 8, 10, 12, 14, 16}
+	result := findClosestElements(array, 3, 10)
+
+	fmt.Println(result)
+}
+
+func findClosestElements(arr []int, k int, x int) []int {
+	left := 0
+	right := len(arr) - 1
+
+	for left < right {
+		mid := (left + right) / 2
+
+		if arr[mid] < x {
+			left = mid
+		} else {
+			right = mid
+		}
+	}
+
+	return arr[left : left+k]
+}
+
+*/
