523853bf70fe73021440d37a32521cd62f311fc3
yuriy <yuriy.cherepnev@gmail.com>
2025-10-16 17:28:52 +0300

    111

--- sandbox/main.go
+++ sandbox/main.go
@@ -3,24 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a0 int = 0
-	var a1 int = 1
-	var a2 int = 2
-	var a3 int = 3
-	var a4 int = 90
-	var a5 int = 45
 
-	var b int = a4 ^ a5 // 110 & 010 = 10  - 2
+	var f func(int, int) int = func(x, y int) int { return x + y }
 
-	fmt.Printf("%b\n", a0)
-	fmt.Printf("%b\n", a1)
-	fmt.Printf("%b\n", a2)
-	fmt.Printf("%b\n", a3)
-	fmt.Printf("%b\n", a4)
-	fmt.Printf("%b\n", a5)
-
-	fmt.Printf("%b\n", b)
-	println(b)
+	fmt.Println(f(3, 4))
 }
 
 /*
