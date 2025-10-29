fee686b73ed5824965726b90ace42c64bef745f0
yuriy <yuriy.cherepnev@gmail.com>
2025-10-29 11:03:43 +0300

    edits

--- sandbox/test.go
+++ sandbox/test.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1)
+
+	var a, b, d int = 1, 2, 4
+
+	p_nums := [4]*int{&a, &b, nil, &d} // массив из 4 указателей на значения типа int
+
+	for _, p := range p_nums {
+		// если указатель не равен nil, выводим значение, которое хранится по его адресу
+		if p != nil {
+			fmt.Println(*p)
+		}
+	}
 }
