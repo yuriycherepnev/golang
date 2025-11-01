415b181d3c76c42c3f7c17de5ba0e7a1d454d0b9
yuriy <yuriy.cherepnev@gmail.com>
2025-11-01 15:35:12 +0300

    111

--- sandbox/test.go
+++ sandbox/test.go
@@ -2,12 +2,27 @@ package main
 
 import "fmt"
 
-var tom struct {
+type person struct { // вложенная структура person
 	name string
 	age  int
 }
 
+type account struct {
+	login    string
+	password string
+
+	personInfo person
+}
+
 func main() {
-	tom.name = "Tom"
-	fmt.Println(tom) // Tom
+	tom := account{
+		login:    "tom@hmail.com",
+		password: "12345678",
+		personInfo: person{
+			name: "Tom",
+			age:  41,
+		},
+	}
+	fmt.Println(tom)
+	fmt.Println("Name: ", tom.personInfo.name) // Name: Tom
 }
