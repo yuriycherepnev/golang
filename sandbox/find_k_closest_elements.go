43c3e6c3bfb2782edd3de8686b02b566886787b4
yuriy <yuriy.cherepnev@gmail.com>
2025-12-03 15:40:05 +0300

    111

--- sandbox/find_k_closest_elements.go
+++ sandbox/find_k_closest_elements.go
@@ -4,10 +4,7 @@ import "fmt"
 
 func main() {
 	array := []int{2, 4, 6, 8, 10, 1000, 1001, 1002, 2000}
-
-	searchIndex := binarySearch(array, 11)
-
-	fmt.Println(array[searchIndex])
+	fmt.Println(array)
 }
 
 func comparison(a int, b int, x int) int {
@@ -27,18 +24,4 @@ func mod(number int) int {
 		return -number
 	}
 	return number
-}
-
-func binarySearch(arr []int, x int) int {
-	i, j := 0, len(arr)-1
-
-	for i < j {
-		midIndex := (i + j) >> 1
-		if arr[midIndex] >= x {
-			j = midIndex
-		} else {
-			i = midIndex + 1
-		}
-	}
-	return i
 }
