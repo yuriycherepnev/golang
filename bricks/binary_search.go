666f4e05bd366a588aedd5464656d640eb612d56
yuriy <yuriy.cherepnev@gmail.com>
2025-12-12 14:55:16 +0300

    111

--- bricks/binary_search.go
+++ bricks/binary_search.go
@@ -1,4 +1,4 @@
-package main
+package bricks
 
 import "fmt"
 
--- sandbox/find_k_closest_elements.go
+++ sandbox/find_k_closest_elements.go
@@ -1,31 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	array := []int{2, 4, 6, 8, 10, 1000, 1002, 1004, 2000}
 
-	i := findClosestElements(array, 3, 0)
-	fmt.Println(i)
+	a := closestBinarySearch(array, 999)
+	fmt.Println(array[a])
 }
 
-func findClosestElements(arr []int, k int, x int) []int {
-	for len(arr) > k {
-		if abs(arr[0]-x) > abs(arr[len(arr)-1]-x) {
-			arr = arr[1:]
+func closestBinarySearch(arr []int, x int) int {
+	i, j := 0, len(arr)-1
+
+	for i+1 < j {
+		mid := (i + j) >> 1
+		if arr[mid] >= x {
+			j = mid
 		} else {
-			arr = arr[:len(arr)-1]
+			i = mid
 		}
 	}
-
-	return arr
+	if mod(arr[i]-x) < (arr[j] - x) {
+		return i
+	}
+	return j
 }
 
-func abs(a int) int {
-	if a < 0 {
-		return -a
+func mod(x int) int {
+	if x < 0 {
+		return -x
 	}
-	return a
+	return x
 }
