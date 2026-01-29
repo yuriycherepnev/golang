edcf94c693aa24d27dac65d0634dfdc87de3a63d
yuriy <yuriy.cherepnev@gmail.com>
2026-01-29 17:17:33 +0300

    333

--- book/simple_routing.go
+++ book/simple_routing.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 func main() {
@@ -13,24 +12,24 @@ func main() {
 	http.ListenAndServe(":8082", nil)
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	query := req.URL.Query()
-	name := query.Get("name")
-	if name == "" {
-		name = "Inigo Montoya"
-	}
-	fmt.Fprint(res, "Hello, my name is ", name)
-}
-
-func goodbye(res http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path
-	parts := strings.Split(path, "/")
-	name := parts[2]
-	if name == "" {
-		name = "Inigo Montoya"
-	}
-	fmt.Fprint(res, "Goodbye ", name)
-}
+//func hello(res http.ResponseWriter, req *http.Request) {
+//	query := req.URL.Query()
+//	name := query.Get("name")
+//	if name == "" {
+//		name = "Inigo Montoya"
+//	}
+//	fmt.Fprint(res, "Hello, my name is ", name)
+//}
+//
+//func goodbye(res http.ResponseWriter, req *http.Request) {
+//	path := req.URL.Path
+//	parts := strings.Split(path, "/")
+//	name := parts[2]
+//	if name == "" {
+//		name = "Inigo Montoya"
+//	}
+//	fmt.Fprint(res, "Goodbye ", name)
+//}
 
 func homePage(res http.ResponseWriter, req *http.Request) {
 	if req.URL.Path != "/" {
