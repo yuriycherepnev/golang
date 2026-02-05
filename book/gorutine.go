85c0e09377d5fada2523301105c46ae28d960261
yuriy <yuriy.cherepnev@gmail.com>
2026-02-05 11:02:03 +0300

    111

--- book/gorutine.go
+++ book/gorutine.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	go echo(os.Stdin, os.Stdout)
+	time.Sleep(30 * time.Second)
+	fmt.Println("Timed out.")
+	os.Exit(0)
+}
+
+func echo(in io.Reader, out io.Writer) {
+	_, err := io.Copy(out, in)
+	if err != nil {
+		return
+	}
+}
