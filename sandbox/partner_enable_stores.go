c17b09cb12a412c78a7e08e21f9e8d0dde4b41ea
yuriy <yuriy.cherepnev@gmail.com>
2026-02-26 12:32:54 +0300

    222

--- sandbox/partner_enable_stores.go
+++ sandbox/partner_enable_stores.go
@@ -1,85 +1,75 @@
+// recovery
 package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"reflect"
 )
 
-type Row struct {
-	PartnerID        int
-	PartnerTitle     string
-	SalePointID      int
-	SalePointAddress sql.NullString
-	AllowDelivery    string
-}
-
-type StoreInfo struct {
-	SalePointID    int
-	SalePointLabel string
-	AllowDelivery  string
-}
-
-type PartnerDelivery struct {
-	PartnerID    int
-	PartnerLabel string
-	Stores       map[int]StoreInfo // key = sale_point_id
-}
+var (
+	ErrDb = errors.New("error db")
+)
 
 func main() {
-	dsn := "root:root@tcp(127.0.0.1:3307)/b2b?charset=utf8mb4&parseTime=true&loc=Local"
-
+	dsn := "root:root@tcp(127.0.0.1:3307)/b2b"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatalf("sql.Open error: %v", err)
+		log.Println(err)
 	}
-	defer db.Close()
-
-	if err = db.Ping(); err != nil {
-		log.Fatalf("db.Ping error: %v", err)
-	}
+	defer func() {
+		err := db.Close()
+		log.Println(err)
+	}()
 
 	query := `
 SELECT
-   partner.id partner_id,
-   partner.title partner_title,
-   sale_point.id sale_point_id,
-   address.address sale_point_address,
-   CASE
-       WHEN partner_disable_store.id IS NULL THEN 'Да'
-       ELSE 'Нет'
-   END allow_delivery
+    partner.id partner_id,
+    partner.title partner_title,
+    sale_point.id sale_point_id,
+    address.address store_address,
+    CASE
+        WHEN partner_disable_store.id IS NULL THEN 'Да'
+        ELSE 'Нет'
+    END enable_store
 FROM partner
 CROSS JOIN sale_point
 LEFT JOIN partner_disable_store
-      ON partner_disable_store.idPartner = partner.id
-     AND partner_disable_store.idSalePoint = sale_point.id
+       ON partner_disable_store.idPartner = partner.id
+      AND partner_disable_store.idSalePoint = sale_point.id
 LEFT JOIN address
-      ON address.id = sale_point.idAddress
+       ON address.id = sale_point.idAddress
 WHERE partner.deleted = 0
+AND sale_point.isActive = 1
 ORDER BY partner.id, sale_point.id;
 `
 	rows, err := db.Query(query)
 	if err != nil {
-		log.Fatalf("db.Query error: %v", err)
+		log.Println(err)
 	}
-
-	fmt.Println(rows)
-	defer func(rows *sql.Rows) {
+	defer func() {
 		err := rows.Close()
-		if err != nil {
+		log.Println(err)
+	}()
 
-		}
-	}(rows)
-
-	t := reflect.TypeOf(rows)
-	// Если это указатель, получаем тип, на который он указывает
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem() // Разыменовываем указатель
+	cols, err := rows.Columns()
+	if err != nil {
+		log.Println(err)
 	}
 
-	fmt.Printf("%T\n", rows)
+	result := make([]interface{}, len(cols))
+	links := make([]interface{}, len(cols))
 
+	for i := range result {
+		links[i] = &result[i]
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(links...); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(result...)
+	}
 }
