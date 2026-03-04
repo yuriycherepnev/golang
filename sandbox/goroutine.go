d929326798776bb68eb5320624f46f34e6d62a0d
yuriy <yuriy.cherepnev@gmail.com>
2026-03-04 12:16:17 +0300

    22

---
+++ sandbox/goroutine.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i < 7; i++ {
+		go func(n int) {
+			result := 0
+			for j := 1; j <= n; j++ {
+				result += j
+			}
+			fmt.Println(n, "-", result)
+		}(i)
+	}
+	fmt.Scanln() // ждем ввода пользователя
+	fmt.Println("The End")
+}
