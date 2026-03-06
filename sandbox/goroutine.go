6449f84e6842a8a3f45c157a84165ccd335147d1
yuriy <yuriy.cherepnev@gmail.com>
2026-03-06 17:49:42 +0300

    666

--- sandbox/goroutine.go
+++ sandbox/goroutine.go
@@ -1,27 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-func work(id int, wg *sync.WaitGroup) {
-
-	fmt.Printf("Горутина %d начала выполнение \n", id)
-	time.Sleep(2 * time.Second) // имитация долгой работы горутины
-	fmt.Printf("Горутина %d завершила выполнение \n", id)
-	wg.Done() // сигнализируем, что горутина завершила работу
-}
+import "fmt"
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(2) // в группе две горутины
+	fmt.Println("Start")
+	fmt.Println(<-createChan(5))
+	fmt.Println("End")
+}
 
-	// вызываем горутины
-	go work(1, &wg)
-	go work(2, &wg)
-
-	wg.Wait() // ожидаем завершения обоих горутин
-	fmt.Println("Горутины завершили выполнение")
+func createChan(n int) chan int {
+	ch := make(chan int)
+	go func() {
+		ch <- n
+	}()
+	return ch
 }
