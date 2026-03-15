bd057fdb7c9fccd38985362781fd845ab493f519
yuriy <yuriy.cherepnev@gmail.com>
2026-03-15 22:32:08 +0300

    111

--- sandbox/test_2.go
+++ sandbox/test_2.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 
-func sum(n uint) uint {
-	var result uint = 0
-	for n != 0 {
-		result += n
-		n -= 1
-	}
-	return result
-}
-
 func main() {
-	fmt.Println(sum(-4))
+
+	str := "aloha"
+
+	for _, value := range str {
+		fmt.Println(value)
+	}
+
 }
