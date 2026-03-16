b9065ab08e803e62187b13248c3482e674442608
yuriy <yuriy.cherepnev@gmail.com>
2026-03-16 13:26:31 +0300

    555

--- sandbox/test_2.go
+++ sandbox/test_2.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	x := make([]int, 3, 5)
+	x := make([]int, 3, 6)
 
-	x = append(x, 1)
-	x = append(x, 2)
+	fmt.Println(x)
+	a := append(x, 1, 2)
+	fmt.Println(x)
+	b := append(x, 4, 5)
+	fmt.Println(x)
 
-	y := append(x, 3)
-	z := append(x, 4)
+	//y := append(x, 3)
+	//z := append(x, 4)
+	//a := append(x, 5)
 
-	fmt.Println(y)
-	fmt.Println(z)
+	fmt.Println(a)
+	fmt.Println(b)
+	//fmt.Println(z)
 }
