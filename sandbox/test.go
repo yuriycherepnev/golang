5d78624b22e7fa89b9c91e502fdfe9f2b46224f3
yuriy <yuriy.cherepnev@gmail.com>
2026-03-26 14:43:09 +0300

    111

--- sandbox/test.go
+++ sandbox/test.go
@@ -1,41 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"github.com/braintree/manners"
-	"net/http"
-	"os"
-	"os/signal"
-)
+import "fmt"
 
 func main() {
-	handler := &handler{}
-	ch := make(chan os.Signal)
-	signal.Notify(ch, os.Interrupt, os.Kill)
-	go listenForShutdown(ch)
-	err := manners.ListenAndServe(":8082", handler)
-	if err != nil {
-		return
-	}
-}
+	seen := make(map[int]bool)
+	seen[2] = true
 
-type handler struct{}
+	fmt.Println(seen[3])
+	fmt.Println(seen[2])
 
-func (h *handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	query := req.URL.Query()
-
-	name := query.Get("name")
-
-	if name == "" {
-		name = "Inigo Montoya"
-	}
-	_, err := fmt.Fprint(res, "Hello, my name is ", name)
-	if err != nil {
-		return
-	}
-}
-
-func listenForShutdown(ch <-chan os.Signal) {
-	<-ch
-	manners.Close()
 }
