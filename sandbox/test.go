efcd83553ffbfc8e85ee00842069c5de0a7f7dd2
yuriy <yuriy.cherepnev@gmail.com>
2026-03-28 17:26:21 +0300

    111

--- sandbox/test.go
+++ sandbox/test.go
@@ -6,6 +6,20 @@ func main() {
 	seen := make(map[int]bool)
 	seen[2] = true
 
-	fmt.Println(8 << 1)
-	fmt.Println(9 << 1)
+	nums := []int{9, 10, 10, 9, 6}
+
+	single := singleNumber(nums)
+
+	fmt.Println(single)
+}
+
+func singleNumber(nums []int) int {
+	result := 0
+	for _, num := range nums {
+		midResult := result ^ num
+		fmt.Println(result, num, midResult)
+		result = midResult
+	}
+
+	return result
 }
